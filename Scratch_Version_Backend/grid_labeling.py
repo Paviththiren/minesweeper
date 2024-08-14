d2b7e26a6490a4360fbe6ccb8c93354d80a2a642
Paviththiren Sivasothilingam <psivasot@student.ethz.ch>
2024-08-14 11:14:11 +0200

    Improved Grid Labeling Algorithm

--- Scratch_Version_Backend/grid_labeling.py
+++ Scratch_Version_Backend/grid_labeling.py
@@ -2,120 +2,17 @@ import numpy as np
 
 def label_grid(grid):
     row,column = np.shape(grid)
-    print(column)
+
+    mine_adjacent_cells = []
 
     for i in range(row):
         for j in range(column):
-            #Check if cell is already a mine, if not start labeling process
-            if grid[i][j] != -1:
-                adjacent_mines = 0
+            if grid[i][j] == -1:
+                mine_adjacent_cells.extend([(i+1,j),(i-1,j),(i,j+1),(i,j-1),(i+1,j+1),(i-1,j-1),(i+1,j-1),(i-1,j+1)])
 
-                if i==0 and j==0:
-                    if grid[i][j+1] ==-1:
-                        adjacent_mines += 1
-                    if grid[i+1][j] ==-1:
-                        adjacent_mines+= 1
-                    if grid[i+1][j+1] ==-1:
-                        adjacent_mines += 1
-
-                elif i==0 and j==column-1:
-                    if grid[i][j-1] ==-1:
-                        adjacent_mines += 1
-                    if grid[i+1][j] ==-1:
-                        adjacent_mines+= 1
-                    if grid[i+1][j-1] ==-1:
-                        adjacent_mines += 1
-
-                elif i== row-1 and j== 0:
-                    if grid[i][j+1] ==-1:
-                        adjacent_mines += 1
-                    if grid[i-1][j] ==-1:
-                        adjacent_mines+= 1
-                    if grid[i-1][j+1] ==-1:
-                        adjacent_mines += 1
-
-                elif i== row-1 and j== column-1:
-                    if grid[i][j-1] ==-1:
-                        adjacent_mines += 1
-                    if grid[i-1][j] ==-1:
-                        adjacent_mines+= 1
-                    if grid[i-1][j-1] ==-1:
-                        adjacent_mines += 1
-
-                elif i == 0 and j!= 0 and j!= column-1:
-                    if grid[i][j - 1] == -1:
-                        adjacent_mines += 1
-                    if grid[i][j + 1] == -1:
-                        adjacent_mines += 1
-                    if grid[i + 1][j] == -1:
-                        adjacent_mines += 1
-                    if grid[i + 1][j + 1] == -1:
-                        adjacent_mines += 1
-                    if grid[i + 1][j - 1] == -1:
-                        adjacent_mines += 1
-
-                elif i == row-1 and j!= 0 and j!= column-1:
-                    if grid[i][j - 1] == -1:
-                        adjacent_mines += 1
-                    if grid[i][j + 1] == -1:
-                        adjacent_mines += 1
-                    if grid[i - 1][j] == -1:
-                        adjacent_mines += 1
-                    if grid[i - 1][j + 1] == -1:
-                        adjacent_mines += 1
-                    if grid[i - 1][j - 1] == -1:
-                        adjacent_mines += 1
-
-                elif j == 0 and i!= 0 and i!= row-1:
-                    if grid[i + 1][j] == -1:
-                        adjacent_mines += 1
-                    if grid[i - 1][j] == -1:
-                        adjacent_mines += 1
-                    if grid[i][j + 1] == -1:
-                        adjacent_mines += 1
-                    if grid[i + 1][j + 1] == -1:
-                        adjacent_mines += 1
-                    if grid[i - 1][j + 1] == -1:
-                        adjacent_mines += 1
-
-                elif j == column-1 and i!= 0 and i!= row-1:
-                    if grid[i + 1][j] == -1:
-                        adjacent_mines += 1
-                    if grid[i - 1][j] == -1:
-                        adjacent_mines += 1
-                    if grid[i][j - 1] == -1:
-                        adjacent_mines += 1
-                    if grid[i + 1][j - 1] == -1:
-                        adjacent_mines += 1
-                    if grid[i - 1][j - 1] == -1:
-                        adjacent_mines += 1
-
-                else:
-                    if grid[i + 1][j] == -1:
-                        adjacent_mines += 1
-                    if grid[i - 1][j] == -1:
-                        adjacent_mines += 1
-                    if grid[i][j - 1] == -1:
-                        adjacent_mines += 1
-                    if grid[i + 1][j - 1] == -1:
-                        adjacent_mines += 1
-                    if grid[i - 1][j - 1] == -1:
-                        adjacent_mines += 1
-                    if grid[i][j + 1] == -1:
-                        adjacent_mines += 1
-                    if grid[i + 1][j + 1] == -1:
-                        adjacent_mines += 1
-                    if grid[i - 1][j + 1] == -1:
-                        adjacent_mines += 1
-                grid[i][j] = adjacent_mines
-
-
-
-
-
-
-
-
-
+    for coordinate in mine_adjacent_cells:
+        if coordinate[0] <= row-1 and coordinate[1] <= column-1 and coordinate[0] >= 0 and coordinate[1] >= 0:
+            if grid[coordinate[0]][coordinate[1]] != -1:
+                grid[coordinate[0]][coordinate[1]] += 1
 
     return grid
--- Scratch_Version_Backend/minesweeper_setup_version_1.py
+++ Scratch_Version_Backend/minesweeper_setup_version_1.py
@@ -2,7 +2,7 @@ import numpy as np
 import random
 from grid_generator import generate_grid
 from grid_labeling import label_grid
-grid = generate_grid(5,5,1)
+grid = generate_grid(5,5,4)
 grid = label_grid(grid)
 print(grid)
 
